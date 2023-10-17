f521c49ad82237c4f772df2ce699bfcef296ebb3
VijayKumarBandi1 <vijaykumar.bandi@indiumsoft.com>
2023-10-17 13:05:34 +0530

    inital commit

---
+++ src/test/java/io/getarrayus/securecaptia/SecurecaptiaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getarrayus.securecaptia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecaptiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
